8e2aec73e593e6fdce5301f49046ce9148758b9d
Ewerton <ewertonduarte37@gmail.com>
2020-01-09 15:14:34 -0300

    Dependências

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+#Install modules requeride
+sudo apt-get install python3-tk
+sudo apt-get install python3-setuptools
+
+#Manager for transfer area
+sudo apt-get install xsel
+
+# packages 
+sudo pip3 install googletrans
+sudo pip3 install keyboard
+sudo pip3 install clipboard
